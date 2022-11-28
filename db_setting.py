faaa55a98d334da08fbf595060660baf083d71c4
Yair Ben David <79565335+Yair-BD@users.noreply.github.com>
2022-11-28 09:15:45 +0200

    add comments to be continue

--- db_setting.py
+++ db_setting.py
@@ -9,13 +9,23 @@ api = Api(app)
 db = SQLAlchemy(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:data1@localhost/track_detections'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:data1@localhost/track_detection'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 class Track(db.Model):
     __tablename__ = 'events' 
     id = db.Column(db.Integer, primary_key=True)
     image_data = db.Column(JSON)
+    
+    # Parsing
+    # date of pushing into db insertts
+    # link to 3b 
+    # date time
+    # camera id 
+    # class
+    # probabilities
+    # x1,2 
+    # y1,2 
    
     def __init__(self, image_data):
         self.image_id = image_data
