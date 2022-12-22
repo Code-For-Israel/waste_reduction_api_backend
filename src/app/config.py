77050fbe48bc31653d9a92a4c387bae826589106
Eran Perelman <73245710+EranPer@users.noreply.github.com>
2022-12-22 11:24:56 +0200

    Added create database if it's not exists

--- src/app/config.py
+++ src/app/config.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 ## if running from docker-compose
 if "DOCKER" in os.environ:
@@ -18,10 +19,46 @@ else:
     _db_pass = 'data1'
     _db_host = 'localhost'
 
+
 ## Building the DB connection string
 class BaseConfig(object):
     DB_NAME = "waste_reduction"
     DB_USER = _db_user
     DB_PASS = _db_pass
     DB_PORT = 5432
+
+    connection = None
+    try:
+        # In PostgreSQL, default username is 'postgres' and password is 'postgres'.
+        # And also there is a default database exist named as 'postgres'.
+        # Default host is 'localhost' or '127.0.0.1'
+        # And default port is '5432'.
+        conn_string = "user=" + DB_USER + " host=" + _db_host + " password=" + DB_PASS + " port='" + str(DB_PORT) + "'"
+        connection = psycopg2.connect(conn_string)
+        print('Database connected.')
+
+    except:
+        print('Database not connected.')
+
+    if connection is not None:
+        connection.autocommit = True
+
+        cur = connection.cursor()
+
+        cur.execute("SELECT datname FROM pg_database;")
+
+        list_database = cur.fetchall()
+
+        if (DB_NAME,) in list_database:
+            print("'{}' Database already exist".format(DB_NAME))
+        else:
+            print("'{}' Database not exist.".format(DB_NAME))
+            # SQL command to create database
+            cur.execute("CREATE DATABASE {};".format(DB_NAME))
+            print('{} database is created.'.format(DB_NAME))
+            # After all operation is done close the database connection and cursor
+            cur.close()
+        connection.close()
+        print('Done')
+
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASS}@{_db_host}:{DB_PORT}/{DB_NAME}'
