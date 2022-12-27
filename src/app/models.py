597f93f79dc46b5a8eac38393d4d1a7e26325b46
Eran Perelman <73245710+EranPer@users.noreply.github.com>
2022-12-27 17:28:28 +0200

    Updated database with Hebrew and coordinates

--- src/app/models.py
+++ src/app/models.py
@@ -10,6 +10,9 @@ class Track(db.Model):
     camera_id = db.Column(db.String)
     image_serial = db.Column(db.String)
     camera_name = db.Column(db.String)
+    camera_name_he = db.Column(db.String)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
     truck_class = db.Column(db.String)
     probability = db.Column(db.Float, nullable=True)
     bbox_cx_fractional = db.Column(db.Float, nullable=True)
@@ -17,13 +20,15 @@ class Track(db.Model):
     bbox_w_fractional = db.Column(db.Float, nullable=True)
     bbox_h_fractional = db.Column(db.Float, nullable=True)
     s3_uri = db.Column(db.String)
-    
 
-    def __init__(self, detection_time, camera_id, camera_name, image_serial, s3_uri, truck_class = None, probability = None, bbox_c_fractional = [None, None, None, None]):
+    def __init__(self, detection_time, camera_id, camera_name, camera_name_he, latitude, longitude, image_serial, s3_uri, truck_class = None, probability = None, bbox_c_fractional = [None, None, None, None]):
         self.datetime = detection_time
         self.camera_id = camera_id
         self.image_serial = image_serial
         self.camera_name = camera_name
+        self.camera_name_he = camera_name_he
+        self.latitude = latitude
+        self.longitude = longitude
         self.truck_class = truck_class
         self.probability = probability
         self.bbox_cx_fractional = bbox_c_fractional[0]
